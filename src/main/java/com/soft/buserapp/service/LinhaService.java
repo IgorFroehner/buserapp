6ac3eb3712be4bd8d1d45aa799394fb75ace5a8d
Machado <elitonmachadod200@gmail.com>
2021-04-02 17:56:08 -0300

    linha fix and test

--- src/main/java/com/soft/buserapp/service/LinhaService.java
+++ src/main/java/com/soft/buserapp/service/LinhaService.java
@@ -1,7 +1,10 @@
 package com.soft.buserapp.service;
 
+import com.soft.buserapp.model.empresa.Empresa;
 import com.soft.buserapp.model.linha.Linha;
 import com.soft.buserapp.model.linha.LinhaRepository;
+import com.soft.buserapp.model.veiculo.Veiculo;
+import com.soft.buserapp.model.veiculo.VeiculoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,9 @@ public class LinhaService {
     @Autowired
     LinhaRepository repository;
 
+    @Autowired
+    VeiculoRepository veiculoRepository;
+
     public Optional<Linha> findById(Long id){
         return this.repository.findById(id);
     }
@@ -24,5 +30,14 @@ public class LinhaService {
 
     public List<Linha> findAll(){
         return this.repository.findAll();
+    }
+
+    public void adicionaOnibusLinha(Linha linha, Veiculo veiculo) {
+        veiculo.setLinha(linha);
+        veiculoRepository.save(veiculo);
+    }
+
+    public List<Linha> linhasDaEmpresa(Empresa empresa) {
+        return this.repository.findAllByEmpresa(empresa);
     }
 }
